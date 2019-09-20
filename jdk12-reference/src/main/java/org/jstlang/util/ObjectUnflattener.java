19b0d29ed11318b311c1af51e358f63b3abc0495
Daniela <danielafragoso@ufl.edu>
2019-09-19 20:42:32 -0400

    Cleaned up ObjectUnflattener logic and added a test for it

--- jdk12-reference/src/main/java/org/jstlang/util/ObjectUnflattener.java
+++ jdk12-reference/src/main/java/org/jstlang/util/ObjectUnflattener.java
@@ -2,32 +2,54 @@ package org.jstlang.util;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
-import com.jayway.jsonpath.Configuration;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import static com.jayway.jsonpath.Option.DEFAULT_PATH_LEAF_TO_NULL;
-import static com.jayway.jsonpath.Option.SUPPRESS_EXCEPTIONS;
+@RequiredArgsConstructor(staticName = "getDefault")
+public class ObjectUnflattener implements Function<Object, Object> {
 
-public class ObjectUnflattener {
-    private static Configuration jsonConfig = Configuration.builder()
-            .options(DEFAULT_PATH_LEAF_TO_NULL, SUPPRESS_EXCEPTIONS).build();
-    public static Map<String,Object> unflatten(Map<String,Object> input) {
+    private final @NonNull String splitEscape = "\\";
+    private final @NonNull String splitterExpression = "(?<!\\\\)\\.";
 
+    /**
+     * This is the entry point to the Object Unflattener logic.
+     * @param input - Any Object
+     * @return - An unflattened version of the Object
+     */
+    public Object apply(Object input) {
+        return this.typeChecker(input);
+    }
+
+    /**
+     * This is the entry point to the Object Unflattener logic.
+     * This method takes a Map and will check if every key in the map can be unflattened based on the splitting criteria.
+     * This action is then performed recursively over every value in the Map, leading to a Depth First traversal and
+     * transformation of the initial input structure
+     * @param input
+     * @return
+     */
+    public Map<String,Object> unflattenMap(Map<String,Object> input) {
         Map<String,Object> unflattenedMap = Maps.newHashMap();
 
         input.forEach( (key,value) -> {
                     Map<String,Object> root = unflattenedMap;
                     // Go through every node in the path and create it if does not exist
                     Object nextValue = null;
-                    List<String> splitKeys = Lists.newArrayList(key.split("\\."));
+                    // This is the regex expression "(?<!\\)\." which will match every "." that does not have a \ in front of it
+                    // this provides a way for the user to be able to use the "." in a key without it being unflattened
+                    List<String> splitKeys = Lists.newArrayList(key.split(splitterExpression));
                     String lastKey = splitKeys.get(splitKeys.size()-1);
                     if(splitKeys.size()>1){
                         splitKeys = splitKeys.subList(0,splitKeys.size()-1);
                         for (String path : splitKeys) {
+                            // recursively iterate over the values
+                            path = path.replace(splitEscape, "");
                             nextValue = root.get(path);
                             if (null == nextValue) {
                                 Map<String, Object> newRoot = Maps.newLinkedHashMap();
@@ -38,26 +60,45 @@ public class ObjectUnflattener {
                             }
                         }
                     }
-                    Object unflattenedValue = ObjectUnflattener.typeChecker(value);
+                    // recursively iterate over the values
+                    Object unflattenedValue = this.typeChecker(value);
+                    // remove escape character from the key
+                    lastKey = lastKey.replace(splitEscape, "");
                     root.put(lastKey, unflattenedValue);
                 });
 
         return unflattenedMap;
     }
 
-    public static Object typeChecker(Object element) {
+    /**
+     * This is a helper method that will check the type of the input and executes
+     * the appropriate logic based on the type
+     *
+     * @param element - An object of any type
+     * @return - Unflattened version of the object
+     */
+    private Object typeChecker(Object element) {
         if(element instanceof Map) {
-            return ObjectUnflattener.unflatten((Map<String,Object>) element);
+            return this.unflattenMap((Map<String,Object>) element);
         }else if (element instanceof Collection) {
-            return ObjectUnflattener.flattenList((Collection<Object>) element);
+            return this.unflattenList((Collection<Object>) element);
         }
 
         return element;
     }
 
-    public static Object flattenList(Collection<Object> collection) {
+    /**
+     * This method iterates over a collection and calls the type checker helper method for every element
+     * in the collection.
+     *
+     * It allows for every element in the collection to be properly unflattened
+     *
+     * @param collection - A collection of objects
+     * @return - A list of unflattened objects
+     */
+    public Object unflattenList(Collection<Object> collection) {
         return collection.stream().map( element ->
-            ObjectUnflattener.typeChecker(element)
+            this.typeChecker(element)
         ).collect(Collectors.toList());
     }
 }
