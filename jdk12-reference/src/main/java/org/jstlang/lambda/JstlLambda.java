1f5d2d7f1f959bde33a133bba91e08b07c0e4f27
horacio23 <horacio.delgado23@gmail.com>
2019-10-08 22:38:25 -0400

    added execution time to the lambda and the html demo

--- jdk12-reference/src/main/java/org/jstlang/lambda/JstlLambda.java
+++ jdk12-reference/src/main/java/org/jstlang/lambda/JstlLambda.java
@@ -2,12 +2,15 @@ package org.jstlang.lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.google.common.collect.Maps;
 import org.jstlang.compiler.JSTLangCompiler;
 import org.jstlang.converters.fasterjackson.FasterJacksonObjectConverter;
 import org.jstlang.domain.definition.ObjectDef;
 import org.jstlang.domain.lambda.JstlLamdaInput;
 import org.jstlang.util.ObjectUnflattener;
 
+import java.math.BigDecimal;
+import java.util.Map;
 import java.util.function.Function;
 
 public class JstlLambda {
@@ -16,7 +19,7 @@ public class JstlLambda {
     private JSTLangCompiler compiler = JSTLangCompiler.newInstance();
 
     public Object jstlHandler(JstlLamdaInput lamdaInput, Context context){
-
+        Long start = System.nanoTime();
         LambdaLogger logger = context.getLogger();
 
         logger.log(lamdaInput.toString());
@@ -30,6 +33,15 @@ public class JstlLambda {
         // apply the mappings
         Object result = mappings.apply(lamdaInput.getSourceObject()) ;
 
-        return result;
+        Long end = System.nanoTime();
+        String timeTaken = BigDecimal.valueOf(end - start).divide(new BigDecimal("1000000")).toString();
+        context.getLogger().log("The JSTL lambda executed successfully,executionTime="+timeTaken);
+
+        // create the output that contains the result and the time taken to process the request
+        Map<String,Object> lambdaOutput = Maps.newLinkedHashMap();
+        lambdaOutput.put("result", result);
+        lambdaOutput.put("executionTime", timeTaken);
+
+        return lambdaOutput;
     }
 }
